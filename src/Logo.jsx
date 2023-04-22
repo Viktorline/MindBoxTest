701c7a14ae1529eaa8d7a7b4b6258434b2877d24
Victor Murzin <viktorlinework@gmail.com>
2023-04-22 09:17:12 +0700

    add default cursor pointer, deployed on vercel

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Logo = () => {
-  return <h1 className='text-center'>todos</h1>;
+  return <h1 className='text-center cursor-default'>todos</h1>;
 };
 
 export default Logo;
